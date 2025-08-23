a9210e57228e61402b8572aee0d29e829f2f6968
Dipeua <dipeuaberthold@gmail.com>
2025-08-23 17:56:00 +0200

    update login page and remote city and appdbcontext

---
+++ Traylo/Migrations/20250823151741_tr.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traylo.Migrations
+{
+    /// <inheritdoc />
+    public partial class tr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeliveryPersonTrackings");
+
+            migrationBuilder.CreateTable(
+                name: "StockSuivis",
+                columns: table => new
+                {
+                    StockSuiviId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeliveryPersonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SI = table.Column<int>(type: "INTEGER", nullable: false),
+                    Entrees = table.Column<int>(type: "INTEGER", nullable: false),
+                    Sorties = table.Column<int>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockSuivis", x => x.StockSuiviId);
+                    table.ForeignKey(
+                        name: "FK_StockSuivis_DeliveryPeople_DeliveryPersonId",
+                        column: x => x.DeliveryPersonId,
+                        principalTable: "DeliveryPeople",
+                        principalColumn: "DeliveryPersonId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StockSuivis_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockSuivis_DeliveryPersonId",
+                table: "StockSuivis",
+                column: "DeliveryPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockSuivis_ProductId",
+                table: "StockSuivis",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockSuivis");
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryPersonTrackings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeliveryPersonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Entrees = table.Column<int>(type: "INTEGER", nullable: false),
+                    Sorties = table.Column<int>(type: "INTEGER", nullable: false),
+                    StockFinal = table.Column<int>(type: "INTEGER", nullable: false),
+                    StockInitial = table.Column<int>(type: "INTEGER", nullable: false),
+                    Total = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryPersonTrackings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeliveryPersonTrackings_DeliveryPeople_DeliveryPersonId",
+                        column: x => x.DeliveryPersonId,
+                        principalTable: "DeliveryPeople",
+                        principalColumn: "DeliveryPersonId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeliveryPersonTrackings_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryPersonTrackings_DeliveryPersonId",
+                table: "DeliveryPersonTrackings",
+                column: "DeliveryPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryPersonTrackings_ProductId",
+                table: "DeliveryPersonTrackings",
+                column: "ProductId");
+        }
+    }
+}
--- Traylo/Migrations/20250823152655_tr2.Designer.cs
+++ Traylo/Migrations/20250823152655_tr2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Traylo.Data;
 
@@ -10,9 +11,11 @@ using Traylo.Data;
 namespace Traylo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250823152655_tr2")]
+    partial class tr2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
